1eeb840440127169b592f4129ea2e1e14137e729
Be-bo <robfiker@gmail.com>
2024-01-21 19:16:51 -0800

    Removed faulty import

--- src/components/Playground/Playground.jsx
+++ src/components/Playground/Playground.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import './Playground.scss';
 import PlaygroundCarousel from '../PlaygroundCarousel/PlaygroundCarousel';
-import OldCombinedCarousel from '../PlaygroundCarousel/OldCombinedCarousel';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
 import GeneratedItem from '../GeneratedItem/GeneratedItem';
