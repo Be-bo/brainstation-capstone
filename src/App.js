6f4a86b31b4644daffed144325433053dccd7632
Be-bo <robfiker@gmail.com>
2024-04-08 01:21:35 -0700

    Removed all unused React components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Account from './components/Account/Account';
 import Playground from './components/Playground/Playground';
 import Gallery from "./components/Gallery/Gallery";
 
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Playground />}></Route>
           <Route path='/playground' element={<Playground />}></Route>
-          <Route path='/account' element={<Account />}></Route> 
           <Route path="/gallery" element={<Gallery/>}></Route>
         </Routes>
       </Router>
